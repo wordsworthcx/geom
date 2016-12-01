b5821c6edce42731b8a4fb78d51834de9b4f1b86
wordsworthcx <wordsworthcx@gmail.com>
2016-12-01 15:14:54 +0800

    update TriangleTest

--- tst/geom/shape/TriangleTest.java
+++ tst/geom/shape/TriangleTest.java
@@ -5,15 +5,35 @@ import geom.shape.Triangle;
 import geom.test.AbstractCaseTest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+import org.mockito.InjectMocks;
 /**
  * @author xuch.
  */
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = {"file:spring-configuration/unit-testing-config.xml"})
 public class TriangleTest extends AbstractCaseTest {
+    @Mock
+    private Triangle mockTriangle;
+
+    @Before
+    public void initialize() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void helloTest() {
+        doReturn(100.00).when(mockTriangle).getArea();
+        mockTriangle.getArea();
+        verify(mockTriangle, atLeastOnce()).getArea();
+        verify(mockTriangle, atLeastOnce()).getPerimeter();
+    }
+
     @Test
     public void happyCaseIsosceles() {
         final double area = 0.5;
@@ -25,7 +45,6 @@ public class TriangleTest extends AbstractCaseTest {
         Assert.assertTrue(triangle.isIsosceles());
         Assert.assertFalse(triangle.isScalene());
     }
-
 
     @Test
     public void happyCaseEquilateral() {
@@ -40,7 +59,6 @@ public class TriangleTest extends AbstractCaseTest {
         Assert.assertTrue(triangle.isIsosceles());
         Assert.assertFalse(triangle.isScalene());
     }
-
 
     @Test
     public void happyCaseScalene() {
@@ -73,8 +91,6 @@ public class TriangleTest extends AbstractCaseTest {
     public void invalidPoints() {
         Triangle triangle = new Triangle(Point2D.ZERO, new Point2D(1, 0), new Point2D(-1, 0));
     }
-
-
 
     private double getTriangleAreaByWidthAndHeight(double width, double height) {
         return width * height / 2.0;
