c2e8b081fe7278080267c36a9690ca14de0d99e5
wordsworthcx <wordsworthcx@gmail.com>
2016-12-01 15:18:58 +0800

    change branch master

--- tst/geom/shape/TriangleTest.java
+++ tst/geom/shape/TriangleTest.java
@@ -5,15 +5,35 @@ import geom.shape.Triangle;
 import geom.test.AbstractCaseTest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+import org.mockito.InjectMocks;
 /**
  * @author xuch.
  */
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = {"file:spring-configuration/unit-testing-config.xml"})
 public class TriangleTest extends AbstractCaseTest {
+    @Mock
+    private Triangle mockTriangle;
+
+    @Before
+    public void initialize() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void helloTest() {
+        doReturn(100.00).when(mockTriangle).getArea();
+        mockTriangle.getArea();
+        verify(mockTriangle, atLeastOnce()).getArea();
+        verify(mockTriangle, atLeastOnce()).getPerimeter();
+    }
+
     @Test
     public void happyCaseIsosceles() {
         final double area = 0.5;
@@ -25,7 +45,6 @@ public class TriangleTest extends AbstractCaseTest {
         Assert.assertTrue(triangle.isIsosceles());
         Assert.assertFalse(triangle.isScalene());
     }
-
 
     @Test
     public void happyCaseEquilateral() {
@@ -40,7 +59,6 @@ public class TriangleTest extends AbstractCaseTest {
         Assert.assertTrue(triangle.isIsosceles());
         Assert.assertFalse(triangle.isScalene());
     }
-
 
     @Test
     public void happyCaseScalene() {
@@ -74,8 +92,6 @@ public class TriangleTest extends AbstractCaseTest {
         Triangle triangle = new Triangle(Point2D.ZERO, new Point2D(1, 0), new Point2D(-1, 0));
     }
 
-
-
     private double getTriangleAreaByWidthAndHeight(double width, double height) {
         return width * height / 2.0;
     }
@@ -83,5 +99,7 @@ public class TriangleTest extends AbstractCaseTest {
     private double getPerimeterOfRightTriangle(double width, double height) {
         return height + width + Math.sqrt(Math.pow(height, 2.0) + Math.pow(width, 2.0));
     }
+
+    //from branch master
 }
 
